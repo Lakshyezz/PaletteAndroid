ed5abd6ad43da3be6b41fe23011cd73d42588e5c
jainl <deep.lakshya1310@gmail.com>
2023-02-24 01:32:27 +0530

    gudStuffDone

--- app/src/main/java/com/example/paletteandroid/MainActivity.kt
+++ app/src/main/java/com/example/paletteandroid/MainActivity.kt
@@ -1,11 +1,63 @@
 package com.example.paletteandroid
 
+import android.graphics.drawable.Drawable
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.graphics.drawable.toBitmap
+import androidx.palette.graphics.Palette
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val image = findViewById<ImageView>(R.id.iv_main)
+        val constraintLayout = findViewById<ConstraintLayout>(R.id.cl_main)
+
+        Glide.with(this)
+            .load("https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/Android_robot.svg/1745px-Android_robot.svg.png")
+            .listener(object : RequestListener<Drawable>{
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    Log.e("TAG","Error loading image",e)
+                    return  false
+                }
+
+                override fun onResourceReady(
+                    resource: Drawable?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    dataSource: DataSource?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    Palette.from(resource!!.toBitmap())
+                        .generate{
+                            palette ->
+                            palette?.let {
+                                val  intColor = it.vibrantSwatch?.rgb ?: 0
+                                if (intColor != null) {
+                                    cl_main.setBackgroundColor(intColor)
+                                }
+                            }
+                        }
+                    return false
+                }
+
+            })
+            .into(image)
     }
 }
